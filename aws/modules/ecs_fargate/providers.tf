dd0c3bb80501c4b7e9fad1d63443d7bf2830187a
jeferson.sousa <jeferson.sousa@redevistorias.com.br>
2023-02-01 19:05:05 -0300

    module create

---
+++ aws/modules/ecs_fargate/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  
+}
+
+provider "aws" {
+  region  = var.region
+  profile    = "terraform"
+
+}
