55255a440b771a69467cc10bc468241d41944b6b
jefersonaraujo <jefersonaraujo95@gmail.com>
2023-02-01 19:10:11 -0300

    module create

---
+++ aws/env/prod/ecs-fargate/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket  = "iac-terraform-iac"
+    key     = "prod/ecs-terraform.tfstate"
+    region  = "us-west-2"
+    profile = "terraform"
+
+
+  }
+}
---
+++ aws/env/prod/ecs-fargate/terraform.tf
@@ -0,0 +1,35 @@
+data "template_file" "container_definitions" {
+  template = file("./container_definitions.json")
+}
+
+module "ecs" {
+  source           = "../../modules/ecs"
+  create_cluster   = true
+  app_count        = 1
+  fargate_cpu      = 256
+  fargate_memory   = 512
+  subnet_ids      = ["subnet-11fc983a", "subnet-41d5391c", "subnet-35415c4c", "subnet-e7a996ac"]
+  vpc_id          = "vpc-b2b131ca"
+  protocol         = "HTTP"
+  family_name      = "frontend"
+  service_name     = "frontend"
+  cluster_name     = "terraform-iac"
+  container1_name  = "frontend"
+  container1_port  = 80
+  container_definitions = data.template_file.container_definitions.rendered
+
+  tags = {
+    Env          = "production"
+    Team         = "tematico-terraform"
+    System       = "app"
+    CreationWith = "terraform"
+  }
+}
+
+output "load_balancer_dns_name" {
+  value = "http://${module.ecs.loadbalance_dns_name}"
+}
+
+output "security_group_id" {
+  value = module.ecs.security_group_id
+}
